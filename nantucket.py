b67f086c134c29a871897c7c4f44872ce9bf6541
Matt Walters <matthewrobertwalters@gmail.com>
2012-09-03 22:57:41 -0700

    nantucket.py generates limericks on loop

--- nantucket.py
+++ nantucket.py
@@ -2,6 +2,8 @@
 import os
 import limerick
 
+print("Welcome to Nantucket!")
+print("loading cmu dictionary...")
 cmudict_path = "cmudict.0.7a.txt"
 if os.path.isfile(cmudict_path):
     cmudict = open(cmudict_path, "r")
@@ -10,5 +12,9 @@ if os.path.isfile(cmudict_path):
             limerick.Word(line)
 
 
-limmerick = limerick.Limmerick()
-limmerick.printOut()
+while True:
+    reply = raw_input("press enter for a limerick. press q to quit.\n")
+    if reply == "q": break
+    limmerick = limerick.Limmerick()
+    limmerick.printOut()
+    print("\n")
